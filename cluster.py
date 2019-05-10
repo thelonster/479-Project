54c923ed31a4f643d453970e504c39cddaebd51f
thelonster <lg3hansen@gmail.com>
2019-05-09 23:58:08 -0700

    Adding data scattering

--- cluster.py
+++ cluster.py
@@ -1,18 +1,17 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import math
-#from mpi4py import MPI
+import numpy as np
+from mpi4py import MPI
 
 df = pd.read_csv('PimaIndians.csv')
 
-#comm = MPI.COMM_WORLD
-#p = comm.Get_size()
-#r = comm.Get_rank()
+comm = MPI.COMM_WORLD
+p = comm.Get_size()
+r = comm.Get_rank()
 
 previousMSE = 0
 MSE = math.inf
-
-df = df.sort_values(by=['test'])
 
 neg = []
 pos = []
@@ -25,8 +24,40 @@ for x in range(1, 392):
 neg = pd.DataFrame.from_dict(neg)
 pos = pd.DataFrame.from_dict(pos)
 
-plt.scatter(neg['glucose'], neg['bmi'], marker='o')
-plt.scatter(pos['glucose'], pos['bmi'], marker='X')
-plt.xlabel('glucose')
-plt.ylabel('bmi')
-plt.show()
+#plt.scatter(neg['glucose'], neg['triceps'], marker='o')
+#plt.scatter(pos['glucose'], pos['triceps'], marker='X')
+#plt.xlabel('glucose')
+#plt.ylabel('diastolic')
+#plt.show()
+
+# Saving the essential data from the DataFrame
+glucose_triceps = df.copy()
+glucose_triceps = glucose_triceps.drop(columns=['pregnant', 'diastolic', 'bmi', 'insulin', 'age', 'diabetes', 'test'])
+glucose_triceps = np.array(glucose_triceps)
+glucose = np.array(df.copy()['glucose'])
+triceps = np.array(df.copy()['triceps'])
+
+def init_centroids(data, k):
+    centroids = data.copy()
+    np.random.shuffle(centroids)
+    return centroids[:k]
+
+buf_size = int(len(glucose_triceps) / p)
+
+# Start K-Means
+# Creating buffers for data to send to proceses
+glubuf = np.zeros(buf_size * 2, dtype='i')
+tribuf = np.zeros(buf_size * 2, dtype='i')
+if r == 0:
+    seeds = init_centroids(glucose_bmi, 2)
+    comm.Bcast(seeds, root=0)
+    comm.Scatter(glucose, glubuf, root=0)
+    comm.Scatter(triceps, tribuf, root=0)
+
+# This removes the extraneous zeros
+glubuf = np.delete(glubuf, np.s_[1::2], 0)
+tribuf = np.delete(tribuf, np.s_[1::2], 0)
+
+while MSE == previousMSE:
+    previousMSE = MSE
+    MSE_t = 0
